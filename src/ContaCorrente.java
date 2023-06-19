c506c42c9dabc4701a9b2e30c515e0e31fd9cbf9
Allan Brito <allbdesenvolvedor04@gmail.com>
2023-06-19 14:43:33 -0300

    Update ContaCorrente.java
    fez burrice, tô consertando

--- src/ContaCorrente.java
+++ src/ContaCorrente.java
@@ -5,7 +5,17 @@ public class ContaCorrente extends Conta {
         super(nome, cpf);
     }
 
-
+    public boolean transferir(Conta destino, double valor) {
+        if (destino == null) {
+            System.out.println("Conta de destino inválida.");
+            return false;
+        }
+        if (this.sacar(valor)) {
+            destino.depositar(valor);
+            return true;
+        }
+        return false;
+    }
 
     public boolean pedirEmprestimo(double valor) {
         if (valor > getSaldo() + (getSaldo() * 0.35) || limitador > 2) {
