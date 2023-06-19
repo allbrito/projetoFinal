57dee91efe5bae00ef3867b7fb427016dc143364
Allan Brito <allbdesenvolvedor04@gmail.com>
2023-06-19 14:49:12 -0300

    Update ContaCorrente.java
    alterando

--- src/ContaCorrente.java
+++ src/ContaCorrente.java
@@ -5,18 +5,6 @@ public class ContaCorrente extends Conta {
         super(nome, cpf);
     }
 
-    public boolean transferir(Conta destino, double valor) {
-        if (destino == null) {
-            System.out.println("Conta de destino inválida.");
-            return false;
-        }
-        if (this.sacar(valor)) {
-            destino.depositar(valor);
-            return true;
-        }
-        return false;
-    }
-
     public boolean pedirEmprestimo(double valor) {
         if (valor > getSaldo() + (getSaldo() * 0.35) || limitador > 2) {
             System.out.println("Limite de empréstimo excedido.");
